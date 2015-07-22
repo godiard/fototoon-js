7eee4e98afad69b75bc983cab9f0732fde758dfc
Gonzalo Odiard <godiard@gmail.com>
2015-07-22 15:51:11 -0300

    Add a empty canvas

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,14 @@
 requirejs.config({
     baseUrl: "lib",
+    shim: {
+        easel: {
+            exports: "createjs"
+        },
+    },
     paths: {
-        activity: "../js"
+        activity: "../js",
+        easel: "../lib/easeljs-0.7.1.min",
+        preload: "../lib/preloadjs-0.4.1.min"
     }
 });
 
